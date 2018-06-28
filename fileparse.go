54001e72548ca880e993bdbc89c84b9cd736fc40
nirwander <nirwander@gmail.com>
2018-06-28 10:47:41 +1000

    file read

--- fileparse.go
+++ fileparse.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+)
 
 func main() {
 	fmt.Println("Hello World!")
+
+	fileBytes, _ := ioutil.ReadFile("C:\\Users\\wander\\go\\xfecr.txt")
+
+	lines := bytes.Split(fileBytes, []byte("\n"))
+
+	for i, line := range lines {
+		fmt.Printf("%d: %s", i, line)
+	}
+
 }
