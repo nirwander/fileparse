8bfc8c1343bae0cfee087b57cb4a4ff1124db0e0
nirwander <nirwander@gmail.com>
2018-07-05 20:31:53 +1000

    1

--- fix.go
+++ fix.go
@@ -14,5 +14,17 @@ func main() {
 	//bb = bytes.Buffer(fileBytes1[:1483])
 	n, _ := bb.Write(fileBytes1[:1483])
 	fmt.Printf("%d bytes buffered\n", n)
-	fmt.Println(bb.Bytes())
+
+	n, _ = bb.Write(fileBytes2[1813:14088])
+	fmt.Printf("%d bytes buffered\n", n)
+
+	n, _ = bb.Write(fileBytes1[16654:])
+	fmt.Printf("%d bytes buffered\n", n)
+
+	err := ioutil.WriteFile(`D:\ry000067444n`, bb.Bytes(), 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	//fmt.Printf("%x/n", bb.Bytes())
 }
